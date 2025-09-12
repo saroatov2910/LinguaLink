68115925dbcf2d1a0cb737caae4d9ec3c714d52e
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-09-13 00:09:44 +0300

    Initialize FastAPI application instance in main_server.py

---
+++ src/main_server.py
@@ -0,0 +1,8 @@
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
+from translation import translate_text
+from connection_manager import ConnectionManager
+
+app = FastAPI()
+    

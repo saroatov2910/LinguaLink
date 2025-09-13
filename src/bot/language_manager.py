73012a854848617e55fed7bb3696736a842f03c1
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-09-13 03:55:49 +0300

    feat: Integrate Firebase Admin SDK for database connection

---
+++ src/bot/language_manager.py
@@ -0,0 +1,7 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+import os
+
+# Initialize Firebase Admin SDK
+SERVICE_ACCOUNT_KEY_PATH = "data/lingualink-d4685-firebase-adminsdk-fbsvc-0e7dcfe69d.json"
+
